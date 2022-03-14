fc01584e925653129e4d2c8996381845c8de01f2
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2022-03-14 19:01:51 +0100

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -50,3 +50,62 @@ void Entity::Move(int dx, int dy)
 	x += dx * speed;
 	y += dy * speed;
 }
+void Entity::SetColliderSize(int x, int y)
+{
+	collider.height = y;
+	collider.width = x;
+}
+
+SDL_Rect Entity::GetColliderRect()
+{
+	SDL_Rect rect;
+
+	rect.x = x;
+	rect.y = y;
+	rect.w = collider.width;
+	rect.h = collider.height;
+
+	return rect;
+}
+
+bool Entity::IsColliding(Entity entity)
+{
+	SDL_Rect rect1 = GetColliderRect();
+	SDL_Rect rect2 = entity.GetColliderRect();
+
+	if (rect1.x <= rect2.x && rect1.w + rect1.x >= rect2.x)
+	{
+		if (rect1.y <= rect2.y && rect1.h + rect1.y >= rect2.y)
+		{
+			return true;
+
+		}
+	}
+
+	if (rect1.x <= rect2.x + rect2.w && rect1.w + rect1.x >= rect2.x + rect2.w)
+	{
+		if (rect1.y <= rect2.y && rect1.h + rect1.y >= rect2.y)
+		{
+			return true;
+
+		}
+	}
+
+	if (rect1.w + rect1.x >= rect2.w + rect2.x && rect1.x <= rect2.w + rect2.x)
+	{
+		if (rect1.h + rect1.y >= rect2.h + rect2.y && rect1.y <= rect2.h + rect2.y) 
+		{
+			return true;
+		}
+	}
+
+	if (rect1.x <= rect2.x && rect1.w + rect1.x >= rect2.x)
+	{
+		if (rect1.h + rect1.y >= rect2.h + rect2.y && rect1.y <= rect2.h + rect2.y)
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
